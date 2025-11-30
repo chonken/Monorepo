64245a8689a5e7a611ee875cdfd9b9524ef2dfec
chonken <liaochonken@gmail.com>
2025-12-01 03:01:49 +0800

    更新

--- scripts/update_storybook.js
+++ scripts/update_storybook.js
@@ -1,11 +1,21 @@
 import { Project } from 'ts-morph'
+import { readdirSync } from 'fs'
+import { join } from 'path'
+
+// 取得id、defaultProps、slots
+const dir = 'packages/components'
+const files = readdirSync(dir, { recursive: true })
+  .filter((f) => f.endsWith('.type.ts'))
+  .map((f) => join(dir, f))
+const modules = await Promise.all(files.map((f) => import(`../${f}`)))
+// OK
+console.log(modules)
+
+// 取得interface
 const project = new Project()
 const sourceFiles = project.addSourceFilesAtPaths(['packages/components/**/*.type.ts', 'packages/layouts/**/*.type.ts'])
-
 for (const sourceFile of sourceFiles) {
-  // 取得interface
   const getType = sourceFile.getInterfaces()[0].getType()
-
   getType.getProperties().forEach((symbol) => {
     const declarations = symbol.getDeclarations()
     const decl = declarations[0]
