6f8ef9f38a77142fba436c8b2995d37b58fff067
chonken <liaochonken@gmail.com>
2025-12-12 15:04:37 +0800

    更新

---
+++ packages/layouts/SidePadding/index.type.ts
@@ -0,0 +1,28 @@
+import type { ElementProps } from '../../type/element'
+
+export const id = 'ch-spd'
+
+export interface SidePaddingProps extends ElementProps {
+  mobile?: '2xl' | 'xl' | 'lg' | 'md' | 'none'
+  mobileFill?: boolean
+  maxWidthStart?: string | number
+  maxWidthEnd?: string | number
+  marginBlock?: string | number
+  paddingBlock?: string | number
+  paddingInline?: string | number
+  minPaddingInline?: string | number
+}
+
+export const props = {
+  element: 'section',
+  maxWidthStart: '100%',
+  maxWidthEnd: '100%',
+  mobile: 'lg',
+  mobileFill: false,
+  paddingBlock: '0',
+  marginBlock: '0',
+  paddingInline: '5%',
+  minPaddingInline: '0',
+}
+
+export const slots = ['default']
