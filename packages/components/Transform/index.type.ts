bfdd709e15ee0f1bdc45abc495cf7d90f2fb90d0
chonken <liaochonken@gmail.com>
2025-12-23 19:37:16 +0800

    更新

---
+++ packages/components/Transform/index.type.ts
@@ -0,0 +1,20 @@
+import { ElementProps } from '../../type/element'
+
+export const id = 'ch-tfm'
+
+export interface TransformProps extends ElementProps {
+  seconds?: string | number
+  translate?: string | number
+  rotate?: string | number
+  scale?: string | number
+  skew?: string | number
+  delay?: string | number
+}
+
+export const props = {
+  element: 'div',
+  seconds: '1',
+  delay: '0',
+}
+
+export const slots = ['default']
---
+++ packages/components/Translate/index.type.ts
@@ -0,0 +1,18 @@
+import { ElementProps } from '../../type/element'
+
+export const id = 'ch-tsl'
+
+export interface TranslateProps extends ElementProps {
+  seconds?: string | number
+  translate?: string | number
+  delay?: string | number
+}
+
+export const props = {
+  element: 'div',
+  seconds: '1',
+  translate: '0 15%',
+  delay: '0',
+}
+
+export const slots = ['default']
