772826551b96ae8b5efc698f4a4eebcc10d65e7f
chonken <liaochonken@gmail.com>
2026-01-08 10:56:13 +0800

    更新

--- packages/components/prototype/ListLine.type.ts
+++ packages/components/prototype/ListLine.type.ts
@@ -3,6 +3,7 @@ import type { ElementProps } from '../../types'
 export default interface ListLineProps extends ElementProps {
   item?: 'stretch' | 'start' | 'center' | 'end'
   gap?: string | number
+  lineGap?: string | number
   trimStart?: boolean
   trimEnd?: boolean
   mobile?: 'lg' | 'md' | 'sm' | 'xs' | '2xs' | 'none'
